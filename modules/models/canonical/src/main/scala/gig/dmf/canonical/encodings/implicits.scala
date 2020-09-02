6ff053bc4ec9a934bae3606968da028fae71d3ba
Michael Ahlers <michael@ahlers.consulting>
2020-09-01 20:39:03 -0400

    Draft canonical marker trait, representation, and conversion

---
+++ modules/models/canonical/src/main/scala/gig/dmf/canonical/encodings/implicits.scala
@@ -0,0 +1,9 @@
+package gig.dmf.canonical.encodings
+
+import Canonical._
+
+/**
+ * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
+ * @since September 01, 2020
+ */
+object implicits extends ImplyEntityAsCanonicalOps with ImplyBlockAsCanonicalOps
