6d1b392278420a8d819d07743758d609eb541065
Michael Ahlers <michael@ahlers.consulting>
2020-09-13 17:56:16 -0400

    Clean up

--- modules/models/encoding-borer/src/main/scala/gig/dmf/canonical/encodings/implicits.scala
+++
@@ -1,9 +0,0 @@
-package gig.dmf.canonical.encodings
-
-import Canonical._
-
-/**
- * @author <a href="mailto:michael@ahlers.consulting">Michael Ahlers</a>
- * @since September 01, 2020
- */
-object implicits extends ImplyEntityAsCanonicalOps with ImplyBlockAsCanonicalOps
