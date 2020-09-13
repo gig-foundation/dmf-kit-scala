c047731d4b80266cd0261d5cdac205d40eaf48fb
Michael Ahlers <michael@ahlers.consulting>
2020-09-13 17:59:35 -0400

    Consolidate around new pattern

--- modules/models/encoding-borer/src/main/scala/gig/dmf/models/encodings/borer/implicits.scala
+++ modules/models/encoding-borer/src/main/scala/gig/dmf/models/encodings/borer/implicits.scala
@@ -1,8 +1,8 @@
 package gig.dmf.models.encodings.borer
 
+import cats._
+import cats.syntax.foldable._
 import gig.dmf.ByteString
-import gig.dmf.canonical.encodings.implicits._
-import gig.dmf.canonical.encodings.Canonical
 import gig.dmf.models.common._
 import io.bullet.borer.Codec
 import io.bullet.borer.Decoder
@@ -17,7 +17,7 @@ import scala.collection.immutable._
  */
 object implicits {
 
-  implicit val EncoderRange: Encoder[Range] =
+  implicit val encoderRange: Encoder[Range] =
     Encoder { (writer, range) =>
       writer.writeArrayOpen(3)
       writer.writeInt(range.start)
@@ -26,7 +26,7 @@ object implicits {
       writer.writeArrayClose()
     }
 
-  implicit val DecoderRange: Decoder[Range] =
+  implicit val decoderRange: Decoder[Range] =
     Decoder { reader =>
       val unbounded = reader.readArrayOpen(3)
       val start = reader.readInt()
@@ -36,14 +36,28 @@ object implicits {
       reader.readArrayClose(unbounded, range)
     }
 
-  implicit val CodecByteString: Codec[ByteString] =
+  implicit val codecByteString: Codec[ByteString] =
     Codec.of[Array[Byte]]
       .bimap(_.toArray, ByteString(_))
 
-  implicit val CodecIdentity: Codec[Identity] = deriveCodec[Identity]
+  implicit val codecIdentity: Codec[Identity] = deriveCodec[Identity]
 
-  implicit val CodecReference: Codec[Reference] = deriveCodec[Reference]
+  implicit val codecReference: Codec[Reference] = deriveCodec[Reference]
 
-  implicit val CodecSelector: Codec[Selector] = deriveCodec[Selector]
+  implicit val codecSelector: Codec[Selector] = deriveCodec[Selector]
+
+  implicit def encoderBlock[F[_]: Foldable](implicit encoders: Has[Encoder[_]]): Encoder[Block[F]] = { (writer, block) =>
+    implicit val encoderElement: Encoder[block.Type] = encoders[Encoder[block.Type]]
+
+    block
+      .elements
+      .foldLeft(writer)(_.write(_))
+  }
+
+  implicit def encoderEntity[F[_]: Foldable](implicit encoders: Has[Encoder[_]]): Encoder[Entity[F]] = { (writer, entity) =>
+    entity
+      .blocks
+      .foldLeft(writer)(_.write(_))
+  }
 
 }
